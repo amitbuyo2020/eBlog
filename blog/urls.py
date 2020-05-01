0d01c9c4fa5fde498b8b2870e7fafa5582b2fa23
Amit Buyo <58477464+amitbuyo2020@users.noreply.github.com>
2020-05-01 10:08:34 +0545

    Links Changed

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,7 @@ from .views import (ArticleListView, ArticleDetailView, ArticleCreateView,
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-home'),
     path('<slug:slug>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('article/new/', ArticleCreateView.as_view(), name='new-article'),
     path('search/', views.searchBar, name='searchBar'),
     path('announcements/', NoticeListView.as_view(), name='announcements'),
     path('article/new/', ArticleCreateView.as_view(template_name='blog/article_form.html'), name='new-article'),
